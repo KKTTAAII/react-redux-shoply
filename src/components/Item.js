05fe24a0ce364b5e34c37463f518b54188fffcf7
KKTTAAII <krataias65@gmail.com>
2022-11-25 17:46:11 -0700

    cartReducer added

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "../css/Item.css";
+import { useDispatch, useSelector } from "react-redux";
+import { add, remove } from "../redux/reducers/actions/actions";
 
 const Item = ({ src, name, price, description }) => {
+  const { cart } = useSelector(state => state.cart);
+  console.log(cart);
+  const dispatch = useDispatch();
+  const addItem = name => dispatch(add(name));
+  const removeItem = name => dispatch(remove(name));
   return (
     <div className="Item-container">
       <img src={src} alt={name} className="Item-img" />
       <p className="Item-name">{name}</p>
       <p className="Item-price">${price}</p>
       <p className="Item-description">{description}</p>
-      <div>
-        <button className="Item-add-btn">➕</button>
-        <button className="Item-remove-btn">➖</button>
+      <div className="Item-update-quantity-container">
+        <button onClick={() => addItem(name)} className="Item-add-btn">
+          ➕
+        </button>
+        <p className="Item-quantity">{cart[name] > 0 ? cart[name] : 0}</p>
+        <button onClick={() => removeItem(name)} className="Item-remove-btn">
+          ➖
+        </button>
       </div>
     </div>
   );
--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,6 +1,24 @@
 const INITIAL_STATE = { cart: {} };
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case "ADD":
+      const addedItem = action.item;
+      if (state.cart[addedItem]) {
+        state.cart[addedItem]++;
+      } else {
+        state.cart[addedItem] = 1;
+      }
+      console.log({ ...state });
+      return { ...state };
+    case "REMOVE":
+      const removedItem = action.item;
+      if (state.cart[removedItem] === 1) {
+        delete state.cart[removedItem];
+      } else {
+        state.cart[removedItem]--;
+      }
+      console.log({ ...state });
+      return { ...state };
     default:
       return state;
   }
